11a55a565f4e0e85d22241b623df934fd176e2b7
HZTPC\xinxibu <279733959@qq.com>
2017-10-17 14:38:03 +0800

    Merge remote-tracking branch 'remotes/origin/develop'
    # Conflicts:
    #	genSjSect_new_2.js

--- genSjSect_new_2.js
+++ genSjSect_new_2.js
@@ -343,7 +343,7 @@ function genSjSect(sjSects, colNum) { //根据传入设计标数组，在表格�
                 <td class="node-cell relLeft">
                     <div class="container-div">
                         <div class="node qt">
-                            <span class="sj-logo4"></span>
+                            <span class="sj-logo"></span>
                             <span data-id="`+sjSects[(sjSects.length-1)].id+`">`+sjSects[(sjSects.length-1)].name+`</span></div>
                         </div>
                     </div>
